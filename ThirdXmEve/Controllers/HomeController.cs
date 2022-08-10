ec36117a65e477f1c712fa93a87593feb30c6be1
nazmulnayem <mdnazmulhasancse072@gmail.com>
2022-08-10 10:55:50 +0600

    blog

---
+++ ThirdXmEve/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThirdXmEve.Models;
+namespace ThirdXmEve.Controllers
+{
+    public class HomeController : Controller
+    {
+        ThirdXmEveEntities db = new ThirdXmEveEntities();
+        public ActionResult Index()
+        {
+            var pst = db.Posts.ToArray();
+            var cat = db.Categories.ToArray();
+
+            ViewBag.posts = pst;
+            ViewBag.categories = cat;
+            return View();
+        }
+        public ActionResult PostByCategory(int? id)
+        {
+            var pst = db.Posts.Where(p => p.CategoryID == id).ToList();
+            var cat = db.Categories.ToArray();
+            ViewBag.postbyid = pst;
+            ViewBag.categories = cat;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ThirdXmEve/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThirdXmEve.Startup))]
+namespace ThirdXmEve
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
